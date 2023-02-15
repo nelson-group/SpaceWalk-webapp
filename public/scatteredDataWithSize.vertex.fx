5993989d086158ae0c7f83922c08d6b059705927
GimlisCode <johannes.maul@gmx.de>
2023-02-15 11:24:25 +0100

    scale point size with zoom... but not schick

--- public/scatteredDataWithSize.vertex.fx
+++ public/scatteredDataWithSize.vertex.fx
@@ -8,6 +8,7 @@ attribute float densities;
 
 uniform mat4 worldViewProjection;
 uniform float distance;
+uniform vec3 cameraPosition;
 
 varying vec2 vUV;
 varying float vdensity;
@@ -16,7 +17,10 @@ void main()
 {
     gl_Position = worldViewProjection * vec4(position, 1.0);    
     // gl_PointSize = max(50. * distance, 1.);  
-    gl_PointSize = 2.;
+    float pointSize = 10.;
+    // gl_PointSize = 10.;
+    // gl_PointSize = pointSize *  max(exp(1. - (length(cameraPosition - position) / distance)) - 1., 0.1);
+    gl_PointSize = pointSize * max(pow(1000000., 1. - (length(cameraPosition - position) / distance)) / 1000000., 0.1);
     vdensity = densities;
     vUV = uv;
 }  
--- src/utils/scenery.ts
+++ src/utils/scenery.ts
@@ -1,5 +1,5 @@
 import { Hdf5File } from "./hdf5_loader";
-import { reshape, min, max, divide, MathType, subtract, norm } from "mathjs";
+import { reshape, min, max, divide, MathType, subtract, norm, abs } from "mathjs";
 import { drawOutline } from "./outline";
 import { drawSpheres } from "./spheres";
 import { calcColor, buildGUI, ColorConfig, useOwnShader } from "./gui";
@@ -8,21 +8,20 @@ import { VectorFieldVisualizer } from "./arrow";
 import { MeshBuilder, StorageBuffer, Scene, PointsCloudSystem, ArcRotateCamera, StandardMaterial, Vector3, Color3, Color4, Engine, CloudPoint, Texture, Vector2, Material, ShaderMaterial, Mesh, Nullable, AxesViewer } from "@babylonjs/core";
 import { AdvancedDynamicTexture } from "@babylonjs/gui";
 
-function setCamera(canvas: HTMLCanvasElement, scene: Scene, cameraPosition: Vector3, targetPosition: Vector3, mesh: Nullable<Mesh> = null) {
+function setCamera(canvas: HTMLCanvasElement, scene: Scene, cameraPosition: Vector3, targetPosition: Vector3, minCoords:Nullable<Array<number>> = null, mesh: Nullable<Mesh> = null) {
     var camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 1.65, 1, cameraPosition, scene);
     camera.setTarget(targetPosition);
     camera.attachControl(canvas, true);
     camera.zoomToMouseLocation = true;
     
-    if (mesh != null)
-    {
-        let nominator = Vector3.Distance(targetPosition, cameraPosition)
+    if (mesh != null && minCoords != null)
+    {           
+        let targetPositionArray = [targetPosition.x, targetPosition.y, targetPosition.z] as Array<number>;
+        let distance = norm(subtract(targetPositionArray, minCoords)) as number;
+        // console.log(distance, minCoords, targetPosition);
         camera.onViewMatrixChangedObservable.add(() => 
-        {        
-            let distance = nominator / Vector3.Distance(targetPosition, camera.globalPosition);            
+        {  
             
-            // console.log(nominator + ":" + Vector3.Distance(targetPosition, camera.globalPosition));
-            console.log(camera._worldMatrix);
             if (useOwnShader)
             {
                 (mesh.material as ShaderMaterial).setFloat("distance", distance);
@@ -94,7 +93,8 @@ export async function createScene(file_url: string, filename: string, partType: 
             scene,
             new Vector3(allCoordsGlobal[1][0], allCoordsGlobal[1][1], allCoordsGlobal[1][2]),
             new Vector3(allCoordsGlobal[0][0], allCoordsGlobal[0][1], allCoordsGlobal[0][2]),
-            pcsMesh        
+            min(allCoordsGlobal, 0),
+            pcsMesh
         );
     }    
 
@@ -102,7 +102,7 @@ export async function createScene(file_url: string, filename: string, partType: 
     arrow.createArrow();
 
     // if (displayOutline) { //not needed anymore since the mesh itself provides boundingboxes
-    //     // drawOutline(allCoordsGlobal, scene);
+    //     drawOutline(allCoordsGlobal, scene);
     // }
 
     return scene;
@@ -127,30 +127,3 @@ function useOwnShaderForMesh(mesh: Mesh, scene: Scene, colorConfig:ColorConfig, 
     mesh.material = shaderMaterial; 
     return tmpMaterial;               
 }
-
-////////////// working shader example ////////////////////////////////
-// var shaderMaterial = new ShaderMaterial("shader", scene, "./scatteredDataWithSize",{                                    
-//     attributes: ["position", "uv","densities"],
-//     uniforms: ["worldViewProjection"]                
-// });                    
-// shaderMaterial.backFaceCulling = false;
-// var mainTexture = new Texture("data/textures/Dot.png", scene);
-
-// shaderMaterial.setTexture("textureSampler", mainTexture);
-
-// var minCoords = min(allCoordsGlobal, 0);
-// var maxCoords = max(allCoordsGlobal, 0);
-
-// var boxSize = subtract(maxCoords, minCoords);
-
-// var centerOfBox = subtract(maxCoords, divide(boxSize, 2));
-
-// var box = MeshBuilder.CreateBox(
-// "shader",
-// { width: boxSize[0], height: boxSize[1], depth: boxSize[2] },
-// scene
-// );            
-// box.material = shaderMaterial;    
-// box.position.x = centerOfBox[0];
-// box.position.y = centerOfBox[1];
-// box.position.z = centerOfBox[2];
