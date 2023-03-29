002926ae64e3f479fc13e033ed7a09b6d80686e2
GimlisCode <johannes.maul@gmx.de>
2023-03-29 19:38:29 +0200

    erster versuch mehrerer snaps...

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,67 @@ var timeConfig = {
     "text_object_interpolation": null,            
     "minimum_fps": 25
 } 
+let call = 0
 
 export class DownloadControl{
   public static downloadInProcess = false
+  public static batch_size_lod = 500;
+
   private static _finishedDownload = false
   private static _downloadHTML = document.getElementById("downloading")!;
+  private static _internalSnapnum = -1
+  private static pcsDictonary:Record<number, Array<PointsCloudSystem>> = {}
+  private static _node_indices:Record<number, Array<number>> = {};
+  private static _level_of_detail:Record<number, Record<number,number>> = {};
+  
+
+  public static get_node_indices(snapNum: number) {
+    if(!this._node_indices[snapNum])
+      this._node_indices[snapNum] = []
+    return this._node_indices[snapNum]
+  }
+
+  public static set_node_indices(new_node_indices:Array<number>,snapNum: number) {  
+      this._node_indices[snapNum] = new_node_indices;
+  }
+
+  public static get_level_of_detail(snapNum: number) {
+    if(!this._level_of_detail[snapNum])
+      this._level_of_detail[snapNum] = {}
+    return this._level_of_detail[snapNum]
+  }
+
+  public static set_level_of_detail(new_level_of_detail:Record<number,number>,snapNum: number) {  
+      this._level_of_detail[snapNum] = new_level_of_detail;
+  }
+
+  public static get internalSnapnum() {
+    return this._internalSnapnum;
+  }
+
+  private static async updatePcsVisibility(oldSnapnum:number, newSnapnum:number)
+  {
+      if(this.pcsDictonary[oldSnapnum])
+        this.pcsDictonary[oldSnapnum].forEach(element => {
+          if (element.mesh)
+            element.mesh.visibility = 0;
+        });
+
+      if(this.pcsDictonary[newSnapnum])
+        this.pcsDictonary[newSnapnum].forEach(element => {
+          if (element.mesh)
+            element.mesh.visibility = 1;
+        });
+  }
+
+  public static set internalSnapnum(newSnapNnum: number) {  
+    if(newSnapNnum != this.internalSnapnum)
+    {
+      this.updatePcsVisibility(this.internalSnapnum, newSnapNnum)
+      this._internalSnapnum = newSnapNnum
+      this.finishedDownload = false
+    }
+  }
 
   public static get finishedDownload() {
     return this._finishedDownload;
@@ -35,9 +91,24 @@ export class DownloadControl{
     this._finishedDownload = finishedDownload;
   }
 
+  public static addPcsToDict(pcs:PointsCloudSystem, forSnap:number)
+  {
+    if(!this.pcsDictonary[forSnap])
+      this.pcsDictonary[forSnap] = new Array<PointsCloudSystem>()
+    this.pcsDictonary[forSnap].push(pcs);
+  }
+
+//   function updateMetaDataOnClient(data: Record<string,any>, current_snapnum:number) {
+//     if (!level_of_detail[current_snapnum])
+//       level_of_detail[current_snapnum] = {}
+//     level_of_detail[current_snapnum] = data.level_of_detail
+
+//     if (!node_indices[current_snapnum])
+//     node_indices[current_snapnum] = new Array<number>
+//     node_indices = data.node_indices    
+// }
   
 }
-
 
 var colorConfig = {
     "min_color": new Color3(0, 0, 0),
@@ -49,17 +120,15 @@ var colorConfig = {
 
 const url = "http://127.0.0.1:8000/v1/get/splines/";
 let simulationName = "TNG50-4"
-let node_indices:Array<number> = [];
-let level_of_detail:Record<number,number> = {};
-let batch_size_lod = 500;
+
 
 async function main() {
     const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
     const divFPS = document.getElementById("fps")!;
     
-
     var engine = new Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-    var [scene, material, guiPanel] = await createScene(canvas, engine, colorConfig, timeConfig, true);
+    var [scene, material, guiPanel] = await createScene(canvas, engine, colorConfig, timeConfig, true);    
+
     let minSlider:Nullable<Slider> = guiPanel.getChildByName("min_opacity_slider") as Slider
     let maxSlider:Nullable<Slider> = guiPanel.getChildByName("max_opacity_slider") as Slider
     window.addEventListener('resize', function () {
@@ -69,14 +138,15 @@ async function main() {
     engine.runRenderLoop(function () {
         divFPS.innerHTML = engine.getFps().toFixed() + " fps";
         scene.render();
-        // if (call < 1)
+
+        DownloadControl.internalSnapnum = timeConfig.current_snapnum;                
         if(!DownloadControl.downloadInProcess && !DownloadControl.finishedDownload)        
         {
           DownloadControl.downloadInProcess = true;
             const payload = {
-                    "node_indices": node_indices,
-                    "level_of_detail": level_of_detail,
-                    "batch_size_lod": batch_size_lod,
+                    "node_indices": DownloadControl.get_node_indices(timeConfig.current_snapnum),
+                    "level_of_detail": DownloadControl.get_level_of_detail(timeConfig.current_snapnum),
+                    "batch_size_lod": DownloadControl.batch_size_lod,
                     "camera_information": CameraConfig.getInstance().getCameraConfig()
               };
             
@@ -103,8 +173,8 @@ async function main() {
                 }
                 // if (call < 1)
                   guiUpdate(data, colorConfig, minSlider, maxSlider, material);                                    
-                  updateMesh(data, material, scene)
-                  updateMetaDataOnClient(data);                  
+                  updateMesh(data, material, scene)                  
+                  updateMetaDataOnClient(data, timeConfig.current_snapnum);                  
                   DownloadControl.downloadInProcess = false;
                 })
                 .catch(error => {
@@ -140,42 +210,34 @@ async function guiUpdate(dataResponse: Record<string,any>, colorConfig: Record<s
     }
 }
 
-function updateMetaDataOnClient(data: Record<string,any>) {
-    level_of_detail = data.level_of_detail
-    node_indices = data.node_indices    
-}
+
 let global_splines_d: Array<any>
 
 function placeholderForShader(particle:CloudPoint, i: number, _s: number){ 
     particle.position = new Vector3(global_splines_d[i * 3], global_splines_d[i * 3 + 1], global_splines_d[i * 3 + 2])
 }
 
-let snapPcs:Record<number,PointsCloudSystem> = {};
-let call = 0
 async function updateMesh(data: Record<string,any>, material: ShaderMaterial, scene:Scene) {         
-
     global_splines_d = data.splines_d;  
-    let pcs2: PointsCloudSystem
-    if (snapPcs[timeConfig.current_snapnum + call])
-      pcs2 = snapPcs[timeConfig.current_snapnum];
-    else
-    {
-      let pcsName = "pcs" + timeConfig.current_snapnum + call;
-      pcs2 = new PointsCloudSystem(pcsName, 20, scene);
-      snapPcs[timeConfig.current_snapnum + call++] = pcs2;
-      pcs2.addPoints(data.nParticles, placeholderForShader);  
-      var pcsMesh = await pcs2.buildMeshAsync(material);
-      pcsMesh.hasVertexAlpha = true;                                                
-      // pcsMesh.showBoundingBox = true;    
-      pcsMesh.setVerticesData("densities", data.densities as FloatArray, false, 2);   
-      pcsMesh.setVerticesData("splinesA", data.splines_a as FloatArray, false, 3);
-      pcsMesh.setVerticesData("splinesB", data.splines_b as FloatArray, false, 3);
-      pcsMesh.setVerticesData("splinesC", data.splines_c as FloatArray, false, 3);      
-    }
+    let pcsName = "pcs" + timeConfig.current_snapnum + call++;
+    let pcs2 = new PointsCloudSystem(pcsName, 20, scene);
+    DownloadControl.addPcsToDict(pcs2, timeConfig.current_snapnum);
+    pcs2.addPoints(data.nParticles, placeholderForShader);  
+    var pcsMesh = await pcs2.buildMeshAsync(material);
+    pcsMesh.hasVertexAlpha = true;                                                
+    // pcsMesh.showBoundingBox = true;    
+    pcsMesh.setVerticesData("densities", data.densities as FloatArray, false, 2);   
+    pcsMesh.setVerticesData("splinesA", data.splines_a as FloatArray, false, 3);
+    pcsMesh.setVerticesData("splinesB", data.splines_b as FloatArray, false, 3);
+    pcsMesh.setVerticesData("splinesC", data.splines_c as FloatArray, false, 3);      
+
  
     console.log(call)
     // var pcsMesh = await pcs.buildMeshAsync();
-
 }
 
+function updateMetaDataOnClient(data: Record<string,any>, current_snapnum: number) {
+  DownloadControl.set_level_of_detail(data.level_of_detail, current_snapnum)
+  DownloadControl.set_node_indices(data.node_indices, current_snapnum)
+}
 
